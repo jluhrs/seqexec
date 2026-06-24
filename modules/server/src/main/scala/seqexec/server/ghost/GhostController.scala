81d27540fd60da1911d0e8cec5d545498ea7cf50
Carlos Quiroz <cquiroz@gemini.edu>
2026-06-24 16:09:47 -0400

    Print the configurations alphabetically

--- modules/server/src/main/scala/seqexec/server/ghost/GhostController.scala
+++ modules/server/src/main/scala/seqexec/server/ghost/GhostController.scala
@@ -57,10 +57,10 @@ object GhostController {
           _          <- Logger[F].info(
                           s"GHOST Guiding state value: ${value.getOrElse("None")}, isGuiding idle: $idle"
                         )
-          _          <- if (idle)
-                          Logger[F].info("Seqexec will send a MOVE_TO to GHOST IFU bFocus and rFocus")
-                        else Applicative[F].unit
-          _          <- Logger[F].debug(pprint.apply(finalConfig).toString)
+          _          <-
+            Logger[F].info("Seqexec will send a MOVE_TO to GHOST IFU bFocus and rFocus").when_(idle)
+          asList      = finalConfig.config.toList.sortBy(_._1)
+          _          <- Logger[F].debug(pprint.apply(asList).toString)
         } yield finalConfig
 
       override def stopObserve: F[Unit] =
